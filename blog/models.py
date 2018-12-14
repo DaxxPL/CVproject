be0cf3c0d710e6b79282f4f6c90cf1a488056fb6
DaxxPL <m.henklewski@gmail.com>
2018-12-14 13:24:57 +0100

    First commit

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.TextField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    created = models.DateTimeField(default=timezone.datetime.now())
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
