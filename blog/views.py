f21986260b80fffcfbc2be6b37b60965cddb9cb2
DaxxPL <m.henklewski@gmail.com>
2018-12-14 14:30:29 +0100

    created post_list page in html without css

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post, Category
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.order_by('created')
+    categories = Category.objects.order_by('name')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'categories': categories})
