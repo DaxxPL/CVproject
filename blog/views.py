512c44d1a5cea67f7c3e5103a656c1d3a17562e3
DaxxPL <m.henklewski@gmail.com>
2018-12-15 17:21:39 +0100

    created css, main page in html

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,4 @@ from .models import Post, Category
 def post_list(request):
     posts = Post.objects.order_by('created')
     categories = Category.objects.order_by('name')
-    return render(request, 'blog/post_list.html', {'posts': posts, 'categories': categories})
+    return render(request, 'blog/post_list.html', {'newest': posts[0], 'posts': posts[1:], 'categories': categories})
