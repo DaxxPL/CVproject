76a19aac4c6161747a4b0798d144760cb326091e
DaxxPL <m.henklewski@gmail.com>
2018-12-16 13:36:24 +0100

    added filtering by category

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('posts/<str:category>', views.post_list_filtered_by_category, name='post_list_filtered_by_category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,10 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     categories = Category.objects.order_by('name')
     return render(request, 'blog/post_detail.html', {'post': post, 'categories': categories})
+
+
+def post_list_filtered_by_category(request, category):
+    c = get_object_or_404(Category, name=category)
+    posts = Post.objects.filter(category=c).order_by('created')
+    categories = Category.objects.order_by('name')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'categories': categories})
