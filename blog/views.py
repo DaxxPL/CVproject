986ab7a0fcf669593eb145fc8499847f14196b5f
DaxxPL <m.henklewski@gmail.com>
2018-12-16 12:19:06 +0100

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 
 
@@ -6,3 +6,9 @@ def post_list(request):
     posts = Post.objects.order_by('created')
     categories = Category.objects.order_by('name')
     return render(request, 'blog/post_list.html', {'newest': posts[0], 'posts': posts[1:], 'categories': categories})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    categories = Category.objects.order_by('name')
+    return render(request, 'blog/post_detail.html', {'post': post, 'categories': categories})
